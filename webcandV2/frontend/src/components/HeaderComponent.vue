ac903a0cef78fe78a8817a83c962ab30abfb99e9
Gui <guillaumecalteau1@gmail.com>
2020-09-04 11:11:11 +0200

    init
    1

---
+++ webcandV2/frontend/src/components/HeaderComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="danger" class="img-responsive">
+      <b-navbar-brand>
+        <div><a href="#">
+          <img id="logoacc" src="../assets/1280px-CNAM_Logo.svg.png">
+        </a></div>
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2"></b-form-input>
+            <b-button
+                size="sm"
+                class="my-2 my-sm-0"
+                type="submit"
+                variant="danger"
+            >Search
+            </b-button
+            >
+          </b-nav-form>
+
+          <b-nav-item-dropdown right>
+            <template v-slot:button-content>
+              <em>Utilisateur</em>
+            </template>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "HeaderComponent"
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
