b610f6dd01d6201cec67c7dcd223740ae1a385b8
Madxyz <ducmega@hotmail.com>
2020-09-04 14:33:55 +0200

    [Back] Service > Enseignants > Formations

---
+++ webcandV2/backend/src/main/java/fr/ipst/webcand/v2/webcand/services/interfaces/IFormationService.java
@@ -0,0 +1,15 @@
+package fr.ipst.webcand.v2.webcand.services.interfaces;
+
+import fr.ipst.webcand.v2.webcand.entities.FormationEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface IFormationService {
+    List<FormationEntity> getAll();
+    FormationEntity findById(Long id);
+    FormationEntity save(FormationEntity formationEntity);
+    FormationEntity update(FormationEntity formationEntity);
+    void deleteById(Long id);
+}
