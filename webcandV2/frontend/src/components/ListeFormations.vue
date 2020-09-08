2f5963c6e4f77c52e2668be94f0ff98867440783
CALLGUI <68611167+CALLGUI@users.noreply.github.com>
2020-09-08 11:34:28 +0200

    Informatique.vue fixes

--- webcandV2/frontend/src/components/ListeFormations.vue
+++ webcandV2/frontend/src/components/ListeFormations.vue
@@ -5,7 +5,6 @@
         text="Liste formation"
         class="d-inline-flex p-2"
         variant="danger">
-
       <b-dropdown-item href="Informatique">Informatique</b-dropdown-item>
       <b-dropdown-item>RH</b-dropdown-item>
       <b-dropdown-item>Gestion</b-dropdown-item>
