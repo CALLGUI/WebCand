ead493917fa12e7bab755313a8b4e5be4fc54dc9
CALLGUI <68611167+CALLGUI@users.noreply.github.com>
2020-09-09 14:47:59 +0200

    [FRONT] InscriptionsInfos hotfix Navigation & routes Login & Sign Up forms

---
+++ webcandV2/frontend/src/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="mx-auto d-flex justify-content-center text-center">
+      <form class="form-signin">
+        <h1 class="h3 mb-3 font-weight-normal display-4">Connectez-vous</h1>
+        <label for="inputEmail" class="sr-only">E-mail</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+        <label for="inputPassword" class="sr-only">Mot de passe</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+
+        <div class="d-inline-block mt-3">
+          <div class="float-left">
+          <label for="save">Souvenez-vous</label>
+          </div>
+          <div class="float-right">
+            <input type="checkbox" id="save" name="save">
+          </div>
+        </div>
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ webcandV2/frontend/src/pages/Postuler.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+<InscriptionInfos/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import InscriptionInfos from "@/components/InscriptionInfos";
+export default {
+name: "Postuler",
+  components: {InscriptionInfos}
+}
+</script>
+
+<style scoped>
+
+</style>
