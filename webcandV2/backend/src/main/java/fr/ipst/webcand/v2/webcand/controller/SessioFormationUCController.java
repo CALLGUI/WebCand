e516567fe175b1368593ad6ea84a6a2c6d6f8989
Madxyz <ahmadzalzali@hotmail.fr>
2020-09-18 02:36:29 +0200

    [BACK]
    Suite UC Creation / Delete de Session d'une Formation

---
+++ webcandV2/backend/src/main/java/fr/ipst/webcand/v2/webcand/controller/SessioFormationUCController.java
@@ -0,0 +1,54 @@
+package fr.ipst.webcand.v2.webcand.controller;
+
+import fr.ipst.webcand.v2.webcand.dto.SessionFormationDto;
+import fr.ipst.webcand.v2.webcand.entities.SessionFormationEntity;
+import fr.ipst.webcand.v2.webcand.mapper.ISessionFormatinMapper;
+import fr.ipst.webcand.v2.webcand.services.SessionFormationUCService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/formations")
+@Tag(name = "Gestion des sessions de formation")
+public class SessioFormationUCController {
+
+    @Autowired
+    private SessionFormationUCService fservice;
+
+    @Autowired
+    private ISessionFormatinMapper imapper;
+
+    /* creation session dans formation */
+    @PostMapping("/{id}/sessionsformation")
+    @Operation(summary = "Méthode permettant de crée une session de la formation.")
+    public ResponseEntity<Void> createSessionFormation(
+            @PathVariable Long id, @RequestBody SessionFormationDto sessionDto) {
+
+        SessionFormationEntity session = fservice.addSession(id,imapper.dtoVersEntite(sessionDto));
+
+        if (session == null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(
+                "/{idSessionFormation}").buildAndExpand(session.getIdSessionFormation()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+
+    @DeleteMapping("/{id}/sessionsformation/{idSession}")
+    @Operation(summary = "Méthode permettant de supprimer une sessions d'une formation.")
+    public void suppressionSessionFormation(
+            @PathVariable("id") Long id, @PathVariable("idSession") Long idSession) {
+
+        fservice.removeSession(id,idSession);
+    }
+
+}
---
+++ webcandV2/backend/src/main/java/fr/ipst/webcand/v2/webcand/services/SessionFormationUCService.java
@@ -0,0 +1,46 @@
+package fr.ipst.webcand.v2.webcand.services;
+
+import fr.ipst.webcand.v2.webcand.entities.FormationEntity;
+import fr.ipst.webcand.v2.webcand.entities.SessionFormationEntity;
+import fr.ipst.webcand.v2.webcand.exception.RessourceNotFoundException;
+import fr.ipst.webcand.v2.webcand.repository.IFormationRepository;
+import fr.ipst.webcand.v2.webcand.repository.ISessionFormationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+public class SessionFormationUCService {
+
+    @Autowired
+    private IFormationRepository formationRepository;
+
+    @Autowired
+    private ISessionFormationRepository sessionRepository;
+
+    public SessionFormationEntity addSession(Long idFormation, SessionFormationEntity session) {
+        FormationEntity formation = formationRepository.findById(idFormation).
+                orElseThrow(()->new RessourceNotFoundException("formation","id","idFormation"));
+
+        if (formation == null) {
+            return null;
+        }
+
+        formation.getSSessionFormationEntity().add(session);
+
+        return session;
+    }
+
+    public void removeSession(Long idFormation, Long idSession) {
+        FormationEntity formation = formationRepository.findById(idFormation).
+                orElseThrow(()->new RessourceNotFoundException("formation","id","idFormation"));
+
+        if (formation == null) {
+            return ;
+        }
+        SessionFormationEntity session = sessionRepository.findById(idSession).
+                orElseThrow(()->new RessourceNotFoundException("session","id","idSession"));
+        formation.getSSessionFormationEntity().remove(session);
+    }
+}
