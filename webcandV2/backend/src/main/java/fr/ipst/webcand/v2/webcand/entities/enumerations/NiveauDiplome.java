d06b4a3fd8609b345bc50717c1a5f527253deec7
Madxyz <ahmadzalzali@hotmail.fr>
2020-09-25 12:52:57 +0200

    [BACK]
    Méthode permettant de retourner le nombre de candidatures total
    @GetMapping("/nombrecandidatures")
    
    Méthode permettant de retourner le nombre de candidatures total d'une session
    @GetMapping("/nombrecandidatures/{idSessions}")
    
    Limitations d'une candidature par candidat
    Ajout un attribut de validation a une candidature
    Ajout un attribut niveau de diplome a un diplome et l'enum correspondant

---
+++ webcandV2/backend/src/main/java/fr/ipst/webcand/v2/webcand/entities/enumerations/NiveauDiplome.java
@@ -0,0 +1,11 @@
+package fr.ipst.webcand.v2.webcand.entities.enumerations;
+
+public enum NiveauDiplome {
+    SansBac,
+    Bac,
+    Bac2,
+    Bac3,
+    Bac4,
+    Bac5,
+    Bac8,
+}
---
+++ webcandV2/backend/src/main/java/fr/ipst/webcand/v2/webcand/entities/enumerations/ValidationAdministrativeStatut.java
@@ -0,0 +1,8 @@
+package fr.ipst.webcand.v2.webcand.entities.enumerations;
+
+public enum ValidationAdministrativeStatut {
+    Valide,
+    Invalide,
+    Refus,
+    En_cours
+}
